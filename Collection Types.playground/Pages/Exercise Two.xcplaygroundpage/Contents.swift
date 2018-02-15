2f5568dd4095e32410ccf852fafddbfe9134cbc9
Lu, Alisa <alu@lcs-mba3124.lcs.on.ca>
2018-02-15 11:29:24 -0500

    Question 4

--- Collection Types.playground/Pages/Exercise Two.xcplaygroundpage/Contents.swift
+++ Collection Types.playground/Pages/Exercise Two.xcplaygroundpage/Contents.swift
@@ -10,22 +10,36 @@
  */
 
 import Foundation
-
+var restaurantRatings : [Double] = []
 
 /*:
  2. Now populate the array with twenty random ratings between 1 and 100.
  */
-
+for _ in 1...20 {
+    restaurantRatings.append( Double( arc4random_uniform(99) + 1 ))
+}
 
 /*:
  3. Determine the average rating. Remember, calculate an average by finding the sum of all the values, then dividing by the number of values.
  */
-
+var totalRating : Double = 0.0
+for rating in restaurantRatings {
+  totalRating += rating
+}
+var average = totalRating / 20
 
 /*:
  4. The restaurant is most concerned with how many diners considered their meal to be less than satisfactory. Calculate the percentage of diners who gave the restaurant a rating less than 80.
  */
+var unsatisfied : Double = 0.0
 
+for rating in restaurantRatings{
+    
+    if rating < 80 {
+       unsatisfied += rating
+    }
+}
+var percentageOfUnsatised = 17 / 20 * %
 /*:
  5. The manager wants more detail. Prepare a report that indicates how many diners found the restuarant "Very Bad" (0 to 25), "Bad" (25 to 49), "Fair" (50 to 79), "Satisfactory" (80 to 89), and "Stellar" (90 to 100).
  */
